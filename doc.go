57ffc50fa1f0c2a2d610cc1146b18e2fb4f145d6
Matt Schultz <schultz@sent.com>
2024-02-04 17:36:45 -0600

    Added simple package-level documentation.

---
+++ doc.go
@@ -0,0 +1,8 @@
+// Copyright 2024 Matt Schultz <schultz@sent.com>. All rights reserved.
+// Use of this source code is governed by an ISC license that can be found in the LICENSE file.
+
+/*
+Package rcon provides mechanisms for interacting with the Source RCON protocol as described by
+Valve Software at https://developer.valvesoftware.com/wiki/Source_RCON_Protocol.
+*/
+package rcon
